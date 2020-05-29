a88a724696c87691efa134e6568aba277a599b1c
chelseachen007 <aqwejie@qq.com>
2020-05-29 09:17:18 +0800

    <docs>算法基础

--- docs/.vuepress/sidebars/leetcode.js
+++ docs/.vuepress/sidebars/leetcode.js
@@ -1,7 +1,12 @@
 module.exports = [
   "/leetcode/",
   {
-    title: "算法",
-    children: ["/leetcode/Leet_one.md"],
+    title: "算法图解",
+    children: [
+      "/leetcode/Grokking_one.md",
+      "/leetcode/Grokking_two.md",
+      "/leetcode/Grokking_three.md",
+      "/leetcode/Grokking_four.md",
+    ],
   },
 ];
