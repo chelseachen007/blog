751db8c4e3f3df4efcef1f2a081cc6bf004730e1
chelseachen007 <aqwejie@qq.com>
2020-05-29 09:44:53 +0800

    <fix>bug

--- docs/.vuepress/sidebars/Animation.js
+++ docs/.vuepress/sidebars/Animation.js
@@ -1,10 +1,7 @@
 // HTTP
 module.exports = [
-    '/Animation/',
-    {
-      title: 'HTTP',
-      children: [
-       '/HTTP/HTTP_one_introduce',
-      ]
-    },
-]
+  "/Animation/",
+  {
+    title: "可视化",
+  },
+];
