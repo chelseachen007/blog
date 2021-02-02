8705731c5223d1d66acf16e5709869756a3264ba
chelsechen <chenjie@souche.com>
2021-02-02 10:20:14 +0800

    feat: 开启算法第三期以及2020年前6本读书笔记

---
+++ docs/leetcode/91day-3/README.md
@@ -0,0 +1,20 @@
+# 目录
+
+### [第一章 - 先导篇](https://github.com/leetcode-pp/91alg-3/blob/master/introduction.md)
+
+- [数据结构与算法概述](https://github.com/leetcode-pp/91alg-3/blob/master/algo.md)
+- [如何衡量算法的性能](https://github.com/leetcode-pp/91alg-3/blob/master/bigO.md)
+- [视频 1](https://www.bilibili.com/video/BV17t4y1i71G)
+- [视频 2](https://www.bilibili.com/video/BV1pa4y1s7g2)
+
+### [第二章 - 基础篇](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic.md)
+
+- [【91 算法-基础篇】01.数组，栈，队列](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic-01.md)
+- [【91 算法-基础篇】02.链表](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic-02.md)
+- [【91 算法-基础篇】03.树](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic-03.md)
+- [【91 算法-基础篇】04.哈希表](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic-04.md)
+- [【91 算法-基础篇】05.双指针](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic-05.md)
+- [【91 算法-基础篇】06.图（加餐）](https://github.com/leetcode-pp/91alg-3/blob/master/lecture/basic-06.md)
+- 【待定】枚举（加餐）
+
+## 做题记录
---
+++ docs/leetcode/91day-3/index.js
@@ -0,0 +1,98 @@
+const fs = require("fs");
+const path = require("path");
+const rootPath = path.join(process.cwd());
+// 遍历所有文件
+const walk = (path) =>
+    fs.readdirSync(path).reduce((files, file) => {
+        // const filePath = path + "/" + file;
+        const stat = fs.statSync(file);
+        if (stat.isFile()) {
+            if (/(.*)\.(md)/.test(file)) {
+                return files.concat(file);
+            }
+        }
+        return files;
+    }, []);
+
+const exists = (filePath) => fs.existsSync(filePath);
+function handleFetchQueue (func, urls, max, callback) {
+    const urlCount = urls.length;
+    const requestsQueue = [];
+    const results = [];
+    const errList = [];
+    let i = 0;
+    const isCallBack = () => {
+        const allLen = results.length + errList.length;
+        if (allLen === urlCount) {
+            "function" === typeof callback && callback(results, errList);
+        }
+    };
+    const handleRequest = (url) => {
+        const req = func(url)
+            .then((res) => {
+                console.log("当前并发： " + requestsQueue);
+                results.push(res);
+                const allLen = results.length + errList.length;
+                if (allLen < urlCount && i + 1 < urlCount) {
+                    requestsQueue.shift();
+                    handleRequest(urls[++i]);
+                } else {
+                    isCallBack();
+                }
+            })
+            .catch((e) => {
+                errList.push(e);
+                isCallBack();
+            });
+        if (requestsQueue.push(req) < max) {
+            handleRequest(urls[++i]);
+        }
+    };
+    handleRequest(urls[i]);
+}
+const func = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileData = fs.readFileSync(file);
+        let fileInfo;
+
+        try {
+            fileInfo = {
+                title: getTitle(fileData.toString()),
+                file
+            }
+        } catch (err) {
+            console.log("读出出错", file);
+            console.log(err);
+            reject({
+                path: file,
+                err: err.toString(),
+            });
+        }
+        resolve(fileInfo);
+    });
+};
+// 提取Markdown标题
+const getTitle = async (fileNode) => {
+    const match = /^#\s?([^#\n\r]*)/.exec(fileNode)
+    return match ? match[1].trim() : fileNode.path
+}
+const SortLikeWin = require('../../utils/utils.js')
+const getReadme = () => {
+    const files = walk(rootPath);
+    const filesList = files.filter(v => parseFloat(v)).sort(SortLikeWin)
+
+    if (!filesList.length) process.exit(2);
+    const fileList = filesList.filter(exists);
+
+    const max = 3;
+    const callback = async (result, errList) => {
+        const mulu = await result.reduce(async (pre, v, i) => {
+            const title = await v.title
+            const prev = await pre
+            return `${prev} \n - [x]  [${title}](./${v.file}) `
+        }, '')
+        fs.writeFileSync("./mulu.md", mulu);
+    };
+    handleFetchQueue(func, fileList, max, callback);
+};
+getReadme()
