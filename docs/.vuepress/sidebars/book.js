77a1df98bc33d712246432654d77111ad2374ed7
chelsechen <chenjie@souche.com>
2021-02-19 17:03:14 +0800

    feat: 读书目录

--- docs/.vuepress/sidebars/book.js
+++ docs/.vuepress/sidebars/book.js
@@ -2,12 +2,19 @@
 module.exports = [
     "/book/",
     {
-        title: "课外阅读",
+        title: "2021课外阅读",
         children: [
-            "/book/1.md",
-            "/book/2.md",
-            "/book/3.md",
-            "/book/4.md",
+            "/book/2021/1.md",
+            "/book/2021/2.md",
+            "/book/2021/3.md",
+            "/book/2021/4.md",
+            "/book/2021/5.md",
+            "/book/2021/6.md",
+            "/book/2021/7.md",
+            "/book/2021/8.md",
+            // "/book/2021/9.md",
+            // "/book/2021/10.md",
+            // "/book/2021/11.md",
         ],
     },
 ];
