5fa7f1950c805840a6b50e1a435c92c2624492be
chelsechen <chenjie@souche.com>
2021-04-07 18:59:10 +0800

    feat: 目录

--- docs/.vuepress/sidebars/Basics.js
+++ docs/.vuepress/sidebars/Basics.js
@@ -8,91 +8,102 @@
  */
 
 module.exports = [
-    "/Basics/",
-    {
-        title: "HTTP",
-        children: [
-            "/Basics/HTTP/HTTP_history.md",
-            "/Basics/HTTP/HTTP_message.md",
-            "/Basics/HTTP/HTTP_cache.md",
-            "/Basics/HTTP/HTTP_https.md",
-            "/Basics/HTTP/HTTP_question.md",
-            "/Basics/HTTP/HTTP_JWT.md",
-        ],
-    },
-    {
-        title: "Chorme",
-        children: [
-            "/Basics/Chrome/Chorme_question.md",
-            "/Basics/Chrome/Chorme_EventLoop.md",
-            "/Basics/Chrome/Chorme_Render.md",
-            "/Basics/Chrome/Chrome_security.md",
-            "/Basics/Chrome/Chrome_V8.md",
-            "/Basics/Chrome/Chrome_session.md",
-        ],
-    },
-    {
-        title: "JavaScript",
-        children: [
-            "/Basics/JavaScript/JS_base.md",
-            "/Basics/JavaScript/JS_DesignPattern.md",
-            "/Basics/JavaScript/JS_typeof.md",
-            "/Basics/JavaScript/JS_module.md",
-            "/Basics/JavaScript/JS_throttle.md",
-            "/Basics/JavaScript/JS_TypeScript.md",
-            "/Basics/JavaScript/JS_HighFunc.md",
-            "/Basics/JavaScript/JS_Reg.md",
-            "/Basics/JavaScript/JS_RedBook.md",
-        ],
-    },
-    {
-        title: "React",
-        children: [
-            "/Basics/React/React_Redux.md",
-            "/Basics/React/React_fiber.md",
-            "/Basics/React/React_hooks.md",
-            "/Basics/React/React_development.md",
-            // "/Basics/React_hooks.md",
-        ],
-    },
-    {
-        title: "Vue",
-        children: [
-            "/Basics/Vue/Vue2_Source.md",
-            "/Basics/Vue/Vue_vue3.md",
-            "/Basics/Vue/Vue_React.md",
-            "/Basics/Vue/Vue_vite.md",
-        ],
-    },
-    {
-        title: "Webpack",
-        children: [
-            "/Basics/Webpack/Webpack_base.md",
-            "/Basics/Webpack/Webpack_optimize.md",
-            "/Basics/Webpack/Webpack_principle.md",
-            "/Basics/Webpack/Webpack_question.md",
-        ],
-    },
-    {
-        title: "前端工程化",
-        children: [
-            "/Basics/Engineer/Engineer_Lint.md",
-            "/Basics/Engineer/Engineer_Docker.md",
-            "/Basics/Engineer/Engineer_Jest.md",
-            "/Basics/Engineer/Engineer_CICD.md",
-            "/Basics/Engineer/Engineer_npm.md",
-            "/Basics/Engineer/Engineer_ALiCloud.md",
-        ],
-    },
-    {
-        title: "项目难点总结",
-        children: [
-            "/Basics/Project/Project_monitorr.md",
-            "/Basics/Project/Project_simple.md",
-            "/Basics/Project/Project.md",
-            // "/Basics/Project/Enginneer_CICD.md",
-            // "/Basics/Project/Enginneer_npm.md",
-            // "/Basics/Project/Engineer_ALiCloud.md",
-        ],
-    },
+  "/Basics/",
+  {
+    title: "HTTP",
+    children: [
+      "/Basics/HTTP/HTTP_history.md",
+      "/Basics/HTTP/HTTP_message.md",
+      "/Basics/HTTP/HTTP_cache.md",
+      "/Basics/HTTP/HTTP_https.md",
+      "/Basics/HTTP/HTTP_question.md",
+      "/Basics/HTTP/HTTP_JWT.md",
+    ],
+  },
+  {
+    title: "Chorme",
+    children: [
+      "/Basics/Chrome/Chorme_question.md",
+      "/Basics/Chrome/Chorme_EventLoop.md",
+      "/Basics/Chrome/Chorme_Render.md",
+      "/Basics/Chrome/Chrome_security.md",
+      "/Basics/Chrome/Chrome_V8.md",
+      "/Basics/Chrome/Chrome_session.md",
+    ],
+  },
+  {
+    title: "JavaScript",
+    children: [
+      "/Basics/JavaScript/JS_base.md",
+      "/Basics/JavaScript/JS_DesignPattern.md",
+      "/Basics/JavaScript/JS_typeof.md",
+      "/Basics/JavaScript/JS_module.md",
+      "/Basics/JavaScript/JS_throttle.md",
+      "/Basics/JavaScript/JS_TypeScript.md",
+      "/Basics/JavaScript/JS_HighFunc.md",
+      "/Basics/JavaScript/JS_Reg.md",
+      "/Basics/JavaScript/JS_RedBook.md",
+    ],
+  },
+  {
+    title: "React",
+    children: [
+      "/Basics/React/React_Redux.md",
+      "/Basics/React/React_fiber.md",
+      "/Basics/React/React_hooks.md",
+      "/Basics/React/React_development.md",
+      "/Basics/React/React_lagou.md",
+      "/Basics/React/React_lagou2.md",
+      // "/Basics/React_hooks.md",
+    ],
+  },
+  {
+    title: "Vue",
+    children: [
+      "/Basics/Vue/Vue2_Source.md",
+      "/Basics/Vue/Vue_vue3.md",
+      "/Basics/Vue/Vue_React.md",
+      "/Basics/Vue/Vue_vite.md",
+    ],
+  },
+  {
+    title: "Webpack",
+    children: [
+      "/Basics/Webpack/Webpack_base.md",
+      "/Basics/Webpack/Webpack_optimize.md",
+      "/Basics/Webpack/Webpack_principle.md",
+      "/Basics/Webpack/Webpack_question.md",
+    ],
+  },
+  {
+    title: "前端工程化",
+    children: [
+      "/Basics/Engineer/Engineer_Lint.md",
+      "/Basics/Engineer/Engineer_Docker.md",
+      "/Basics/Engineer/Engineer_Jest.md",
+      "/Basics/Engineer/Engineer_CICD.md",
+      "/Basics/Engineer/Engineer_npm.md",
+      "/Basics/Engineer/Engineer_ALiCloud.md",
+    ],
+  },
+  {
+    title: "计算机原理",
+    children: [
+      "/Basics/System/base.md",
+      "/Basics/System/Brew.md",
+      "/Basics/System/github.md",
+    ],
+  },
+  {
+    title: "项目难点总结",
+    children: [
+      "/Basics/Project/Project_monitorr.md",
+      "/Basics/Project/Project_simple.md",
+      "/Basics/Project/Project.md",
+      "/Basics/Project/youhua.md",
+      // "/Basics/Project/Enginneer_CICD.md",
+      // "/Basics/Project/Enginneer_npm.md",
+      // "/Basics/Project/Engineer_ALiCloud.md",
+    ],
+  },
 ];
